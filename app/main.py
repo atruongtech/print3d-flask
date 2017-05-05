df201cd66cccea1056f8e9619add637ee1afeed1
Alan <atruongcontact@gmail.com>
2017-05-05 13:44:32 -0700

    added self-signed cert and hosting logic to test with ssl

--- app/main.py
+++ app/main.py
@@ -327,4 +327,4 @@ class PrintDetailResp(Resource):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, ssl_context=('./certs/server.crt', './certs/server.key'))
